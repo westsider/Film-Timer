9dfab2efc780da7cfa90cbe7c4854b91a64b0378
Warren Hansen <whansen1@mac.com>
2017-10-11 16:25:23 -0700

    Initial Commit

---
+++ Timer test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer test
+//
+//  Created by Warren Hansen on 10/11/17.
+//  Copyright © 2017 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
